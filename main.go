9d0660e75fd2bd14285c501795236514fe05de8b
Stanislav Vitko <stas@vitko.me>
2018-12-11 10:34:59 -0800

    clean helloworld headers

--- main.go
+++ main.go
@@ -1,13 +1,6 @@
-// Copyright 2018 Google Inc. All rights reserved.
-// Use of this source code is governed by the Apache 2.0
-// license that can be found in the LICENSE file.
-
-// [START gae_go111_app]
-
-// Sample helloworld is an App Engine app.
+// Sample app showing issue with GAE -> google spreadsheets
 package main
 
-// [START import]
 import (
 	"context"
 	"fmt"
@@ -18,9 +11,6 @@ import (
 	"golang.org/x/oauth2/google"
 	"google.golang.org/api/sheets/v4"
 )
-
-// [END import]
-// [START main_func]
 
 func main() {
 	http.HandleFunc("/", indexHandler)
@@ -37,16 +27,9 @@ func main() {
 	// [END setting_port]
 }
 
-// [END main_func]
-
-// [START indexHandler]
-
 // indexHandler responds to requests with our greeting.
 func indexHandler(w http.ResponseWriter, r *http.Request) {
 	ctx := context.Background()
-	// For packages whose import path is starting with "google.golang.org/api",
-	// such as google.golang.org/api/cloudkms/v1, use the
-	// golang.org/x/oauth2/google package as shown below.
 	client, _ := google.DefaultClient(ctx, "https://www.googleapis.com/auth/spreadsheets.readonly")
 	srv, err := sheets.New(client)
 
@@ -70,6 +53,3 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 }
-
-// [END indexHandler]
-// [END gae_go111_app]
